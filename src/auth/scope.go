d5f839cb1c0fcf3312bdbd377fc081ffdcb5c262
Ajinx1 <haryortorlarh@gmail.com>
2026-06-12 13:45:47 +0100

    refactor: consolidate scope logic by merging ScopeTaxpayerQuery into ScopeQuery with variadic column support

--- src/auth/scope.go
+++ src/auth/scope.go
@@ -8,22 +8,23 @@ import (
 	"gorm.io/gorm"
 )
 
-func (c *Claims) ScopeTaxpayerQuery(
-	db *gorm.DB,
-	column string,
-) (*gorm.DB, error) {
-
-	if !c.IsTaxpayer() {
-		return db, nil
-	}
-
-	return db.Where(fmt.Sprintf(`%s = ?`, column), c.GetTaxID()), nil
-}
-
 func (c *Claims) ScopeQuery(
 	db *gorm.DB,
-	column string,
+	staffColumn string,
+	taxpayerColumn ...string,
 ) (*gorm.DB, error) {
+
+	if c.IsTaxpayer() {
+
+		if len(taxpayerColumn) == 0 {
+			return db, nil
+		}
+
+		return db.Where(
+			fmt.Sprintf(`%s = ?`, taxpayerColumn[0]),
+			c.GetTaxID(),
+		), nil
+	}
 
 	switch {
 
@@ -38,16 +39,23 @@ func (c *Claims) ScopeQuery(
 			return nil, errors.New("regional user has no assigned offices")
 		}
 
-		return db.Where(fmt.Sprintf(`%s = ANY(?)`, column), pq.Array(officeIDs)), nil
+		return db.Where(
+			fmt.Sprintf(`%s = ANY(?)`, staffColumn),
+			pq.Array(officeIDs),
+		), nil
 
 	case c.IsOffice():
+
 		officeID := c.GetOfficeIDString()
 
 		if officeID == "" {
 			return nil, errors.New("office user has no assigned office")
 		}
 
-		return db.Where(fmt.Sprintf(`%s = ?`, column), officeID), nil
+		return db.Where(
+			fmt.Sprintf(`%s = ?`, staffColumn),
+			officeID,
+		), nil
 
 	default:
 		return nil, errors.New("invalid user coverage level")
